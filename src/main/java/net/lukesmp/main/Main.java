be0ba134eb4ffc779b0e0c51598c9b2fd12d537a
Luke Saccento <76127645+SmexyLuke@users.noreply.github.com>
2022-07-15 04:06:09 -0700

    Initial commit

---
+++ src/main/java/net/lukesmp/main/Main.java
@@ -0,0 +1,221 @@
+package net.lukesmp.main;
+
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.Random;
+
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this,this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+    }
+
+    Random rand = new Random();
+
+    @EventHandler
+    public void playerChatEvent(PlayerChatEvent event){
+        String player=event.getPlayer().getDisplayName();
+        String chat=event.getMessage();
+        event.setFormat(ChatColor.DARK_AQUA+player+ChatColor.RESET+" "+ChatColor.GRAY+ChatColor.BOLD+ChatColor.MAGIC+"|"+ChatColor.RESET+" "+ChatColor.RESET+ChatColor.GRAY+chat);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        Player player=event.getPlayer();
+
+        //tab header/footer
+        player.setPlayerListHeader(ChatColor.DARK_AQUA+""+ChatColor.BOLD+"LukeSMP"+ChatColor.GRAY+""+ChatColor.BOLD+""+ChatColor.MAGIC+" | "+ChatColor.DARK_PURPLE+"Season 4");
+        player.setPlayerListFooter(ChatColor.GRAY+""+ChatColor.BOLD+ChatColor.MAGIC+" | "+ChatColor.RED+"mc.lukesmp.net"+ChatColor.GRAY+ChatColor.BOLD+ChatColor.MAGIC+" | ");
+
+        //title at join
+        player.sendTitle(ChatColor.DARK_AQUA+""+ChatColor.BOLD+"Luke"+ChatColor.DARK_PURPLE+""+ChatColor.BOLD+"SMP",ChatColor.RED+"Season 4");
+
+        //change join message
+        event.setJoinMessage(ChatColor.DARK_AQUA+"Luke"+ChatColor.DARK_PURPLE+"SMP"+ChatColor.RESET+" "+ChatColor.GRAY+ChatColor.BOLD+ChatColor.MAGIC+"|"+ChatColor.RESET+" "+ChatColor.RESET+ChatColor.GREEN+player.getDisplayName()+ChatColor.GRAY+" has joined");
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event){
+        Player player=event.getPlayer();
+
+        //change quit message
+        event.setQuitMessage(ChatColor.DARK_AQUA+"Luke"+ChatColor.DARK_PURPLE+"SMP"+ChatColor.GRAY+""+ChatColor.BOLD+ChatColor.MAGIC+" | "+ChatColor.RED+player.getDisplayName()+ChatColor.GRAY+" has left");
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event){
+        for(Player player : Bukkit.getOnlinePlayers()){
+            if((player.getLocation().getBlockX() >= -521) && (player.getLocation().getBlockX() <= -520)){
+                if((player.getLocation().getBlockY() == 67)){
+                    if((player.getLocation().getBlockZ() >= -180) && (player.getLocation().getBlockZ() <= -179)){
+//                        Location loc = new Location(Bukkit.getWorld("world"), -523.5, 67, -177.5, 0, 0);
+                        Location loc = player.getLocation().add(0, 1, 0);
+                        player.teleport(loc);
+                        player.setHealth(0);
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player=event.getEntity().getPlayer();
+        Location l = event.getEntity().getLocation();
+        World w = event.getEntity().getWorld();
+        String deathMessage=event.getDeathMessage();
+        Location deathLocation = event.getEntity().getLocation();
+
+        if (event.getEntity().getKiller() != null){
+            String killer=event.getEntity().getKiller().getDisplayName();
+            String deathMessage1=deathMessage.replace(player.getDisplayName(), ChatColor.RED+player.getDisplayName()+ChatColor.GRAY);
+            String deathMessage2=deathMessage1.replace(killer, ChatColor.GREEN+killer+ChatColor.GRAY);
+            String deathMessage3=deathMessage2.replace("[",ChatColor.AQUA+"[");
+            event.setDeathMessage(ChatColor.DARK_AQUA+"Luke"+ChatColor.DARK_PURPLE+"SMP"+ChatColor.RESET+" "+ChatColor.GRAY+ChatColor.BOLD+ChatColor.MAGIC+"|"+ChatColor.RESET+" "+deathMessage3);
+        } else{
+            String deathMessage1=deathMessage.replace(player.getDisplayName(), ChatColor.RED+player.getDisplayName()+ChatColor.GRAY);
+            String deathMessage2=deathMessage1.replace("[",ChatColor.AQUA+"[");
+            event.setDeathMessage(ChatColor.DARK_AQUA+"Luke"+ChatColor.DARK_PURPLE+"SMP"+ChatColor.RESET+" "+ChatColor.GRAY+ChatColor.BOLD+ChatColor.MAGIC+"|"+ChatColor.RESET+" "+deathMessage2);
+        }
+
+        if((deathLocation.getBlockX() >= -521) && (player.getLocation().getBlockX() <= -520)){
+            if((deathLocation.getBlockY() == 68)){
+                if((deathLocation.getBlockZ() >= -180) && (player.getLocation().getBlockZ() <= -179)){
+                    event.setDeathMessage(ChatColor.DARK_AQUA+"Luke"+ChatColor.DARK_PURPLE+"SMP"+ChatColor.RESET+" "+ChatColor.GRAY+ChatColor.BOLD+ChatColor.MAGIC+"|"+ChatColor.RED+" "+player.getDisplayName()+ChatColor.GRAY+" recieved a JUG");
+                }
+            }
+        }
+
+        //SmexyLuke death
+        if (player.getUniqueId().toString().equals("46257261-7468-4a8b-bb32-b7f5a78f7a0a")){
+            w.dropItemNaturally(l, new ItemStack(Material.GUNPOWDER, rand.nextInt(4)));
+        }
+        //Light_5_Squid death
+        if (player.getUniqueId().toString().equals("32eb72fb-daa3-4f9d-91b6-82031daac3a3")){
+            if (player.hasPotionEffect(PotionEffectType.GLOWING)){
+                w.dropItemNaturally(l, new ItemStack(Material.GLOW_INK_SAC, rand.nextInt(4)));
+            } else{
+                w.dropItemNaturally(l, new ItemStack(Material.INK_SAC, rand.nextInt(4)));
+            }
+        }
+        //ankleBowl death
+        if (player.getUniqueId().toString().equals("f9f3cc81-df7e-4f99-ab6c-72967f526b51")){
+            w.dropItemNaturally(l, new ItemStack(Material.BOWL, rand.nextInt(4)));
+        }
+        //FruityMaybe death
+        if (player.getUniqueId().toString().equals("2c1f5eaf-cc7e-4016-a836-05a81e300ca4")){
+            w.dropItemNaturally(l, new ItemStack(Material.APPLE, rand.nextInt(4)));
+        }
+
+    }
+
+    @EventHandler
+    public void entityDamageEvent(EntityDamageEvent event){
+        Entity entity=event.getEntity();
+        if (entity instanceof Tameable){
+            Tameable tameable=(Tameable) entity;
+            if (tameable.isTamed()){
+                if (tameable.getHealth()-event.getFinalDamage()<=0){
+                    tameable.remove();
+                    event.setCancelled(true);
+                    String ownerName=tameable.getOwner().getName();
+                    String petName=tameable.getName();
+                    String message="";
+                    switch (event.getCause()){
+                        case BLOCK_EXPLOSION:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" blew up";
+                            break;
+                        case CONTACT:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" shouldn't have touched that";
+                            break;
+                        case CRAMMING:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" decided it was too crowded";
+                            break;
+                        case DRAGON_BREATH:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" was breathed on too hard by a dragon";
+                            break;
+                        case DROWNING:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" learned the hard way that they couldn't breathe underwater";
+                            break;
+                        case ENTITY_ATTACK:
+                        case ENTITY_SWEEP_ATTACK:
+                        case THORNS:
+                            Entity killer = tameable.getKiller();
+                            if (killer instanceof Player){
+                                message=ChatColor.RED+petName+ChatColor.GRAY+" was slain by "+ChatColor.RED+killer;
+                            }else{
+                                message=ChatColor.RED+petName+ChatColor.GRAY+" was slain";
+                            }
+                            break;
+                        case ENTITY_EXPLOSION:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" was blown up by a creeper, aww man";
+                            break;
+                        case FALL:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" realized they couldn't fly";
+                            break;
+                        case FALLING_BLOCK:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" was hit in the head by a falling block";
+                            break;
+                        case FIRE:
+                        case FIRE_TICK:
+                        case LAVA:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+""+ChatColor.BOLD+" HATES "+ChatColor.RESET+""+ChatColor.GRAY+ownerName+" so they burned to death";
+                            break;
+                        case FREEZE:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" tried to swim in powdered snow";
+                            break;
+                        case HOT_FLOOR:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" discovered that the floor is too hot";
+                            break;
+                        case LIGHTNING:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" met their end due to Thor";
+                            break;
+                        case MAGIC:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" went Avada Kedavra";
+                            break;
+                        case PROJECTILE:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" was shot";
+                            break;
+                        case SUFFOCATION:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" can only breathe air";
+                            break;
+                        case SUICIDE:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+""+ChatColor.BOLD+" HATES "+ChatColor.RESET+""+ChatColor.GRAY+ownerName+" so they committed suicide";
+                            break;
+                        case VOID:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" fell into the nothing";
+                            break;
+                        case WITHER:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" died a slow and painful death due to withering";
+                            break;
+                        default:
+                            message=ChatColor.RED+petName+ChatColor.GRAY+" died";
+                            break;
+                    }
+                    Bukkit.broadcastMessage(ChatColor.DARK_AQUA+"Luke"+ChatColor.DARK_PURPLE+"SMP"+ChatColor.RESET+" "+ChatColor.GRAY+ChatColor.BOLD+ChatColor.MAGIC+"|"+ChatColor.RESET+" "+message);
+                }
+            }
+        }
+    }
+}
