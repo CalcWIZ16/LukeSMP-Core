3da4f60fdb40f8895d6d9fa1e840105e663075bf
Luke Saccento <76127645+CalcWIZ16@users.noreply.github.com>
2024-10-20 17:30:23 -0700

    checks if recipe is already present

--- src/main/java/net/lukesmp/lukesmpcore/CustomTrades.java
+++ src/main/java/net/lukesmp/lukesmpcore/CustomTrades.java
@@ -2,70 +2,46 @@ package net.lukesmp.lukesmpcore;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.WanderingTrader;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Merchant;
 import org.bukkit.inventory.MerchantInventory;
 import org.bukkit.inventory.MerchantRecipe;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class CustomTrades implements Listener {
 
-//    @EventHandler
-//    public void onInventoryOpen(InventoryOpenEvent event) {
-//        Bukkit.getConsoleSender().sendMessage("Inventory opened");
-//        if(event.getInventory() instanceof MerchantInventory) {
-//            Bukkit.getConsoleSender().sendMessage("instance of merchant");
-//            MerchantInventory inventory = (MerchantInventory) event.getInventory();
-//            Merchant merchant = inventory.getMerchant();
-//
-//            ArrayList<MerchantRecipe> newRecipes = new ArrayList<>();
-//            for(MerchantRecipe recipe : merchant.getRecipes()) {
-//                newRecipes.add(recipe);
-//            }
-//
-//            if(inventory.getHolder() == null) {
-//                return;
-//            }
-//
-//            MerchantRecipe firstCustomRecipe = new MerchantRecipe(new ItemStack(Material.END_STONE, 8), 8);
-//            firstCustomRecipe.addIngredient(new ItemStack(Material.EMERALD, 1));
-//
-//            newRecipes.add(firstCustomRecipe);
-//
-//            merchant.setRecipes(newRecipes);
-//
-//        }
-//    }
-
     @EventHandler
-    public void onEntitySpawn(EntitySpawnEvent event) {
-        if(event.getEntity() instanceof WanderingTrader) {
-            WanderingTrader trader = (WanderingTrader) event.getEntity();
-            MerchantInventory inventory = (MerchantInventory) trader.getInventory();
-            Merchant merchant = inventory.getMerchant();
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        Bukkit.getConsoleSender().sendMessage("Inventory opened");
 
-            ArrayList<MerchantRecipe> newRecipes = new ArrayList<>();
-            for(MerchantRecipe recipe : merchant.getRecipes()) {
-                newRecipes.add(recipe);
-            }
+
+        if(event.getInventory() instanceof MerchantInventory) {
+            MerchantInventory inventory = (MerchantInventory) event.getInventory();
+            Merchant merchant = inventory.getMerchant();
 
             if(inventory.getHolder() == null) {
                 return;
             }
 
-            MerchantRecipe firstCustomRecipe = new MerchantRecipe(new ItemStack(Material.END_STONE, 8), 8);
-            firstCustomRecipe.addIngredient(new ItemStack(Material.EMERALD, 1));
+            List<MerchantRecipe> recipes = merchant.getRecipes();
+            boolean customRecipeExists = false;
+            for(MerchantRecipe recipe : recipes) {
+                if(recipe.getResult().getType() == Material.END_STONE && recipe.getResult().getAmount() == 8) {
+                    customRecipeExists = true;
+                    break;
+                }
+            }
 
-            newRecipes.add(firstCustomRecipe);
-
-            merchant.setRecipes(newRecipes);
+            if (!customRecipeExists) {
+                MerchantRecipe firstCustomRecipe = new MerchantRecipe(new ItemStack(Material.END_STONE, 8), 8);
+                firstCustomRecipe.addIngredient(new ItemStack(Material.EMERALD, 1));
+                recipes.add(firstCustomRecipe);
+                merchant.setRecipes(recipes);
+            }
         }
     }
 }
