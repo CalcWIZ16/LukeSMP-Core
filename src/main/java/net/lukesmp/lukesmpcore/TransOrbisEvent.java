da3b692552ec568fc5caffe556bb1499e514fd69
Luke Saccento <76127645+CalcWIZ16@users.noreply.github.com>
2024-12-14 22:53:06 -0800

    optimized unlock checker

--- src/main/java/net/lukesmp/lukesmpcore/TransOrbisEvent.java
+++ src/main/java/net/lukesmp/lukesmpcore/TransOrbisEvent.java
@@ -164,7 +164,7 @@ public class TransOrbisEvent implements Listener {
     }
 
     private boolean unlockCheck(Block keyLocation, Material baseBlock) {
-        if (keyLocation.getType() == Material.BEACON) {
+        if (keyLocation.getType() == baseBlock) {
             for (int x = -1; x <= 1; x++) {
                 for (int z = -1; z <= 1; z++) {
                     if (keyLocation.getRelative(x, -1, z).getType() != baseBlock) {
